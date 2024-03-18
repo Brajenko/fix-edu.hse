78fea7327d0c477c82feec15e01fdfd16a59c3c3
Brajenko <goshabrajenko@gmail.com>
2024-03-18 13:31:38 +0300

    create readble titles feature

--- src/scripts/AutoOpenLink.js
+++ src/scripts/AutoOpenLink.js
@@ -1,7 +1,6 @@
 function AutoOpenLink() {
     const url = document.querySelector('.urlworkaround').querySelector('a').href;
     window.open(url);
-    // chrome.tabs.create({url: url});
 }
 
 AutoOpenLink()
